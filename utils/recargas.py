066bd9c7332339486f743a1606173c547b8ad58e
anhangueraspdados-coder <anhangueraspdados@gmail.com>
2025-11-21 16:36:31 -0300

    Update recargas.py

--- utils/recargas.py
+++ utils/recargas.py
@@ -1,5 +1,4 @@
-from utils.bigquery_client import get_bq_client
+from utils.bigquery_client import registrar_recarga
 
-def listar_recargas(id_chip):
-    sql = f"SELECT * FROM `painel-universidade.marts.f_recarga` WHERE id_chip={id_chip} ORDER BY data_recarga DESC"
-    return list(get_bq_client().query(sql).result())
+def adicionar_recarga(sk_chip, valor, data, origem="painel", observacao=""):
+    return registrar_recarga(sk_chip, valor, data, origem, observacao)

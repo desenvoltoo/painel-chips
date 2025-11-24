c4319992e62d86237a3708cc42751831311f75d5
anhangueraspdados-coder <anhangueraspdados@gmail.com>
2025-11-24 09:06:51 -0300

    Update chips.py

--- utils/chips.py
+++ utils/chips.py
@@ -1,7 +1,24 @@
-from utils.bigquery_client import listar_chips, listar_eventos_chip
+from google.cloud import bigquery
 
-def get_chips():
-    return listar_chips()
+PROJECT = "painel-universidade"
+DATASET = "marts"
+TABLE = f"{PROJECT}.{DATASET}.dim_chip"
 
-def get_chip_eventos(sk_chip):
-    return listar_eventos_chip(sk_chip)
+client = bigquery.Client()
+
+def listar_chips():
+    sql = f"SELECT * FROM `{TABLE}` ORDER BY sk_chip DESC"
+    return list(client.query(sql).result())
+
+def inserir_chip(data):
+    rows = [{
+        "iccid": data.get("iccid"),
+        "numero_chip": data.get("numero_chip"),
+        "operadora": data.get("operadora"),
+        "status_chip": data.get("status_chip"),
+        "data_ativacao": data.get("data_ativacao"),
+        "data_ultima_recarga": data.get("data_ultima_recarga"),
+        "valor_ultima_recarga": float(data.get("valor_ultima_recarga") or 0)
+    }]
+
+    client.insert_rows_json(TABLE, rows)

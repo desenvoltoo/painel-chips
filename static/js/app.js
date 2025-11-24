d2792c15f499e4b6f4560f8fc013abf2a20a3c65
anhangueraspdados-coder <anhangueraspdados@gmail.com>
2025-11-24 09:17:35 -0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,10 @@
-console.log('Painel Chips carregado');
+console.log("Painel Chips & Aparelhos carregado");
+
+/* 
+   Aqui você pode colocar funções globais do painel, 
+   animações, filtros, etc.
+*/
+
+function msgToast(txt) {
+    alert(txt);
+}

5bfcc6415ac6de10359062a939695f7906900b38
anhangueraspdados-coder <anhangueraspdados@gmail.com>
2025-11-25 11:12:14 -0300

    Update recargas.py

--- utils/recargas.py
+++ utils/recargas.py
@@ -1,4 +1,4 @@
-from utils.bigquery_client import registrar_recarga
+from utils.bigquery_client import BigQueryClient
 
 def adicionar_recarga(sk_chip, valor, data, origem="painel", observacao=""):
     return registrar_recarga(sk_chip, valor, data, origem, observacao)

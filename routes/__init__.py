37ab677e884b64e3a2b26d6d21fe5fb69b59d687
desenvoltoo <matheuscosta.tec@icloud.com>
2025-12-02 11:23:43 -0300

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,13 +4,12 @@
 # IMPORTAÇÃO DOS BLUEPRINTS
 # ============================
 
-from .auth import auth_bp          #  <-- FALTAVA
 from .chips import chips_bp
 from .aparelhos import aparelhos_bp
 from .recargas import recargas_bp
 from .relacionamentos import relacionamentos_bp
 from .dashboard import bp_dashboard
-from .admin import admin_bp
+
 
 
 # ============================
@@ -19,11 +18,6 @@ from .admin import admin_bp
 
 def register_blueprints(app):
 
-    # Autenticação (login/logout)
-    app.register_blueprint(auth_bp)   # <-- ESSÊNCIA DO LOGIN FUNCIONAR
-
-    # Painel administrativo
-    app.register_blueprint(admin_bp)
 
     # Dashboard geral
     app.register_blueprint(bp_dashboard)
@@ -33,3 +27,4 @@ def register_blueprints(app):
     app.register_blueprint(aparelhos_bp)
     app.register_blueprint(recargas_bp)
     app.register_blueprint(relacionamentos_bp)
+

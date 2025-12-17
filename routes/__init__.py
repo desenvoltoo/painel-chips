78fbf4af248b64954c4d04a45ef085586bdc28bb
desenvoltoo <matheuscosta.tec@icloud.com>
2025-12-17 13:24:48 -0300

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,14 +1,16 @@
+# routes/__init__.py
 # -*- coding: utf-8 -*-
 
-# ============================
-# IMPORTAÇÃO DOS BLUEPRINTS
-# ============================
+def register_blueprints(app):
+    # IMPORTA AQUI DENTRO (lazy import) PRA EVITAR CIRCULAR IMPORT
+    from .dashboard import bp_dashboard
+    from .aparelhos import aparelhos_bp
+    from .chips import chips_bp
+    from .relacionamentos import relacionamentos_bp
+    from .movimentacao import mov_bp  # se você tiver
 
-from .chips import chips_bp
-from .aparelhos import aparelhos_bp
-from .recargas import recargas_bp
-from .relacionamentos import relacionamentos_bp
-from .dashboard import bp_dashboard
-
-
-
+    app.register_blueprint(bp_dashboard)
+    app.register_blueprint(aparelhos_bp)
+    app.register_blueprint(chips_bp)
+    app.register_blueprint(relacionamentos_bp)
+    app.register_blueprint(mov_bp)
